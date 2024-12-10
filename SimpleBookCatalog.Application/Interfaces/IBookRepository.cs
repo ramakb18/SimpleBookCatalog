3fb421ec687d8117238466421ab69bee8d1b8c45
RA <RA@PROELE29>
2024-12-10 14:24:16 +0100

    Add project files.

---
+++ SimpleBookCatalog.Application/Interfaces/IBookRepository.cs
@@ -0,0 +1,17 @@
+﻿using SimpleBookCatalog.Domain.Entities;
+
+namespace SimpleBookCatalog.Application.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task AddAsync(Book book);
+
+        Task<List<Book>> GetAllAsync(); 
+
+        Task<Book?> GetByIdAsync(int id);
+
+        Task UpdateAsync(Book book);
+
+        Task DeleteByIdAsync(int id);
+    }
+}
---
+++ SimpleBookCatalog.Domain/Entities/Book.cs
@@ -0,0 +1,25 @@
+﻿using SimpleBookCatalog.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleBookCatalog.Domain.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Please provide a title!")]
+        [StringLength(100)]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage ="Please provide the author's name!")]
+        [StringLength(100)]
+        public string? Author { get; set; }
+        public DateTime? PublicationDate { get; set; }
+
+        [EnumDataType(typeof(Category),ErrorMessage ="Please select a category!")]
+        public Category Category { get; set; }
+
+
+
+    }
+}
